c4a68505b19ae861593a8beb4a8cac11885fb836
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-06-08 18:13:00 +0200

    Add more examples and change Gradient types

--- example/App.js
+++ example/App.js
@@ -1,13 +1,19 @@
 // @flow
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {StyleSheet, ScrollView, SafeAreaView} from 'react-native';
 import Heart from './components/Heart';
+import CustomShape from './components/CustomShape';
+import CustomText from './components/CustomText';
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Heart />
-    </ScrollView>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <CustomText />
+        <Heart />
+        <CustomShape />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
