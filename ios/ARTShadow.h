8ddbab6e83ec2eb7b80ed849d9119503c97e60cd
Alan Kenyon <alan@shipt.com>
2019-12-18 11:16:43 -0500

    update comment

--- ios/ARTShadow.h
+++ ios/ARTShadow.h
@@ -1,9 +1,9 @@
-//
-//  ARTShadow.h
-//  ART
-//
-//  Created by Alan Kenyon on 12/5/19.
-//
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 typedef struct {
   CGSize offset;
